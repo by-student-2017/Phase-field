cc2e9ad6691331f9d4b5dbe227d706e37463a956
by-student-2017 <studentsctest@gmail.com>
2022-08-22 16:59:00 +0900

    ubumtu 20.04 lts

--- Koyama_lab_nu/3D_programs/pfm2/3DAPT_MPF_v3/3DAPT_MPF_v3.cpp
+++ Koyama_lab_nu/3D_programs/pfm2/3DAPT_MPF_v3/3DAPT_MPF_v3.cpp
@@ -101,14 +101,15 @@ int main(void)
 	//
 	//double ch[(nstep+1)*nstep]; // [(nstep+1)*nstep]=[nstep*nstep+nstep], a[i][j]= a[i*n+j] for a[][n]
 	//a[z * ySize * xSize + y * xSize + x], a[i][j][k]=a[i*n*m + j*n + k]
-	double *ph   = (double *)malloc(sizeof(double)*( N*ND*ND*ND + ND*ND*ND + ND*ND + ND ));	//ph[1][i][j]～ph[n][i][j]:位置[i][j]におけるpf
-	int    *qh   =       (int *)malloc(sizeof(int)*( N*ND*ND*ND + ND*ND*ND + ND*ND + ND ));	//qh[1][i][j]～qh[n][i][j]:位置[i][j]においておける粒番号
+	//bag fix: change from N*ND*ND*ND to GNP*ND*ND*ND
+	double *ph   = (double *)malloc(sizeof(double)*( GNP*ND*ND*ND + ND*ND*ND + ND*ND + ND ));	//ph[1][i][j]～ph[n][i][j]:位置[i][j]におけるpf
+	int    *qh   =       (int *)malloc(sizeof(int)*( GNP*ND*ND*ND + ND*ND*ND + ND*ND + ND ));	//qh[1][i][j]～qh[n][i][j]:位置[i][j]においておける粒番号
 	int    *n00  =       (int *)malloc(sizeof(int)*( ND*ND*ND + ND*ND + ND ));	//位置[i][j]においてphが0ではない場合の個数
 	int    *n00p =       (int *)malloc(sizeof(int)*( ND*ND*ND + ND*ND + ND ));	//位置[i][j]におけるphおよびその周辺のphが0ではない場合の個数
 	// n00[i*ND*ND+j*ND+k], n00p[i*ND*ND+j*ND+k]
 	//
-	double *ph2  = (double *)malloc(sizeof(double)*( N*ND*ND*ND + ND*ND*ND + ND*ND + ND ));	// ph2[kk*ND*ND*ND+i*ND*ND+j*ND+k];
-	int    *qh2  =       (int *)malloc(sizeof(int)*( N*ND*ND*ND + ND*ND*ND + ND*ND + ND ));	// qh2[kk*ND*ND*ND+i*ND*ND+j*ND+k];
+	double *ph2  = (double *)malloc(sizeof(double)*( GNP*ND*ND*ND + ND*ND*ND + ND*ND + ND ));	// ph2[kk*ND*ND*ND+i*ND*ND+j*ND+k];
+	int    *qh2  =       (int *)malloc(sizeof(int)*( GNP*ND*ND*ND + ND*ND*ND + ND*ND + ND ));	// qh2[kk*ND*ND*ND+i*ND*ND+j*ND+k];
 	//
 	double *aij  = (double *)malloc(sizeof(double)*( GNP*GNP + GNP ));	// aij[i][j]= aij[i*GNP+j]
 	double *wij  = (double *)malloc(sizeof(double)*( GNP*GNP + GNP ));	// wij[i][j]= wij[i*GNP+j]
