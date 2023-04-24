8e0bd475b2ccd8ca14b988595bdf11bc1efb14a2
by-student-2017 <studentsctest@gmail.com>
2023-04-24 10:29:08 +0900

    ubuntu 22.04 lts

---
+++ Biner_lab_inl/chapter7/dynamic_memory/pfc_2d_mpi_omp/write_vtk_grid_values_2D.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h> //rand()
+#include <math.h> //M_PI
+
+void write_vtk_grid_values_2D(int nx, int ny, 
+	double dx, double dy,
+	int istep, double *data1){
+	
+	//open output file
+	char fname[256];
+	sprintf(fname,"time_%d.vtk",istep);
+	FILE *out=fopen(fname,"w");
+	
+	int nz=1;
+	int npoin=nx*ny*nz;
+	
+	//start writing ASCII VTK file
+	
+	//header of VTK file
+	fprintf(out,"# vtk DataFile Version 3.0 \n");
+	fprintf(out,"time_10.vtk \n");
+	fprintf(out,"ASCII \n");
+	fprintf(out,"DATASET STRUCTURED_POINTS \n");
+	
+	//coords of grid points
+	fprintf(out,"DIMENSIONS %5d %5d %5d \n",nx,ny,nz);
+	fprintf(out,"ORIGIN 0.0 0.0 0.0 \n");
+	fprintf(out,"ASPECT_RATIO 1.0 1.0 1.0 \n");
+	
+	//write grid point values
+	fprintf(out,"POINT_DATA %15d \n",npoin);
+	fprintf(out,"SCALARS DEN float 1 \n");
+	fprintf(out,"LOOKUP_TABLE default \n");
+	
+	for(int j=0;j<ny;j++){
+		for(int i=0;i<nx;i++){
+			//ii=i*ny+j;
+			//fprintf(out,"%14.6e \n",data1[i][j]);
+			fprintf(out,"%14.6e \n",data1[i*ny+j]);
+		}
+	}
+	
+	fclose(out);
+	return;
+}
