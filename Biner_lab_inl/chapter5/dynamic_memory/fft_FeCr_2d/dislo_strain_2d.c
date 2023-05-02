6b0222478c0cc7775550ada667bd195e8737e776
by-student-2017 <studentsctest@gmail.com>
2023-05-02 10:43:14 +0900

    ubuntu 22.04 lts

--- Biner_lab_inl/chapter5/dynamic_memory/fft_FeCr_2d/dislo_strain_2d.c
+++ Biner_lab_inl/chapter5/dynamic_memory/fft_FeCr_2d/dislo_strain_2d.c
@@ -2,9 +2,6 @@
    dislocations to the simulation cell. idislo=1 is for
    a dipole and idislo=2 is for dislocation array */
 
-#include <stdlib.h> //rand()
-#include <fftw3.h>
-
 /* Variable and array list
   Nx: Number of grid points in the x-direction
   Ny: Number of grid points in the y-direction
@@ -16,7 +13,7 @@
 */
 
 void dislo_strain_2d(int Nx, int Ny, int idislo,
-	fftw_complex *ed11, fftw_complex *ed22, fftw_complex *ed12){
+	double *ed11, double *ed22, double *ed12){
 	
 	int ii;
 	
@@ -24,14 +21,14 @@ void dislo_strain_2d(int Nx, int Ny, int idislo,
 		for(int j=0;j<Ny;j++){
 			ii=i*Ny+j;
 			//
-			ed11[ii][0] = 0.0;
-			ed11[ii][1] = 0.0;
+			ed11[ii] = 0.0;
+			ed11[ii] = 0.0;
 			//
-			ed22[ii][0] = 0.0;
-			ed22[ii][1] = 0.0;
+			ed22[ii] = 0.0;
+			ed22[ii] = 0.0;
 			//
-			ed12[ii][0] = 0.0;
-			ed12[ii][1] = 0.0;
+			ed12[ii] = 0.0;
+			ed12[ii] = 0.0;
 		}
 	}
 	
@@ -44,8 +41,7 @@ void dislo_strain_2d(int Nx, int Ny, int idislo,
 		for(int i=0;i<Nx;i++){
 			//if(i>=34 && i<=94){
 			if(i>=ndipoles && i<=ndipolee){
-				ed12[i*Ny+Ny2][0]=5.0e-3;
-				ed12[i*Ny+Ny2][1]=0.0;
+				ed12[i*Ny+Ny2]=5.0e-3;
 			}
 		}
 	}//end if
@@ -64,8 +60,7 @@ void dislo_strain_2d(int Nx, int Ny, int idislo,
 					if(i>=ndipoles && i<ndipolee){
 						if(j==jj){
 							ii=i*Ny+j;
-							ed12[ii][0]=5.0e-3;
-							ed12[ii][1]=0.0;
+							ed12[ii]=5.0e-3;
 						}
 					}
 				}//j
