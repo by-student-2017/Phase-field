958ec4791a55c1a9ebb8359109587b322eac704d
by-student-2017 <studentsctest@gmail.com>
2023-05-11 11:11:50 +0900

    ubuntu 22.04 lts

--- Takaki_lab_kit/ubuntu_version/Accelerate_phase-field_simulation_with_GPU/main-gpu.cu
+++ Takaki_lab_kit/ubuntu_version/Accelerate_phase-field_simulation_with_GPU/main-gpu.cu
@@ -10,97 +10,135 @@
 
 __global__ void Kernel
 (
-	float *f, float *fn, int nx, int ny,
-	float rr, float temp, float L0,
-	float kapa_c, float da, float db, float dt, float dx, float dy
+	float *f,float *fn, int nx, int ny,
+	float  rr, float temp,float L0,
+	float kapa_c,float da,float db,float dt,float dx,float dy
 )
 {
 	int j, jx, jy;
+	//----- ----- ----- ----- ----- ----- ----- ----- ----- ----- 
+	jx = blockDim.y*blockIdx.y + threadIdx.y;
+	jy = blockDim.x*blockIdx.x + threadIdx.x;
+	j  = nx*jy + jx;
+	//----- ----- ----- ----- ----- ----- ----- ----- ----- ----- 
+	
 	float fcc, fce, fcw, fcs, fcn, fcnw, fcne, fcsw, fcse, fcww, fcee, fcnn, fcss, 
 		  mu_chc, mu_chw, mu_che, mu_chn, mu_chs, mu_suc, mu_suw, mu_sue, mu_sun, 
 		  mu_sus, mu_c, mu_w, mu_e, mu_n, mu_s,  
 		  nab_mu, dfmdx, dfmdy, dab = db/da, mcc, dmc, dfdt ;
-	
-	jx = blockDim.y*blockIdx.y + threadIdx.y;
-	jy = blockDim.x*blockIdx.x + threadIdx.x;
-	j  = nx*jy + jx;
-	
-	fcc = f[j];
-	
+	//----- ----- ----- ----- ----- ----- ----- ----- ----- ----- 
+	/* Consider up to the second nearest neighbor. Therefore, 
+	   13 difference grid points are used. 
+	   The if statement is used because of periodic boundary conditions.*/
+	//----- ----- ----- ----- ----- ----- ----- ----- ----- ----- #1
+	fcc = f[j]; // concentration at current point
+	//----- ----- ----- ----- ----- ----- ----- ----- ----- ----- #2
 	if(jx == 0)    fcw = f[j+nx-1];
 	else           fcw = f[j   -1];
+	//----- ----- ----- ----- ----- ----- ----- ----- ----- ----- #3
 	if(jx == nx-1) fce = f[j-nx+1];
 	else           fce = f[j   +1];
+	//----- ----- ----- ----- ----- ----- ----- ----- ----- ----- #4
 	if(jy == 0)    fcs = f[j+nx*ny-nx];
 	else           fcs = f[j      -nx];
+	//----- ----- ----- ----- ----- ----- ----- ----- ----- ----- #5
 	if(jy == ny-1) fcn = f[j-nx*ny+nx];
 	else           fcn = f[j      +nx];
-	
+	//----- ----- ----- ----- ----- ----- ----- ----- ----- ----- #6
 	if(jx == 0 && jy == ny-1)     { fcnw = f[nx-1];}
 	else if(jx == 0 && jy  < ny-1){ fcnw = f[j+nx    +nx-1];}
 	else if(jx  > 0 && jy == ny-1){ fcnw = f[j-nx*ny +nx-1];}
 	else                          { fcnw = f[j       +nx-1];}
-	
+	//----- ----- ----- ----- ----- ----- ----- ----- ----- ----- #7
 	if(jx == nx-1 && jy  < ny-1)     { fcne = f[j-nx    +nx+1];}
 	else if(jx  < nx-1 && jy == ny-1){ fcne = f[j-nx*ny +nx+1];}
 	else if(jx == nx-1 && jy == ny-1){ fcne = f[0];}
 	else                             { fcne = f[j       +nx+1];}
-	
+	//----- ----- ----- ----- ----- ----- ----- ----- ----- ----- #8
 	if(jx == 0 && jy >  0)     { fcsw = f[j+nx    -nx-1];}
 	else if(jx  > 0 && jy == 0){ fcsw = f[j+nx*ny -nx-1];}
 	else if(jx == 0 && jy == 0){ fcsw = f[nx*ny-1];} 
 	else                       { fcsw = f[j       -nx-1];}
-	
+	//----- ----- ----- ----- ----- ----- ----- ----- ----- ----- #9
 	if(jx == nx-1 && jy == 0)     {fcse = f[nx*ny-1-nx+1];}
 	else if(jx == nx-1 && jy  > 0){ fcse = f[j-nx    -nx+1];}
 	else if(jx <  nx-1 && jy == 0){ fcse = f[j+nx*ny -nx+1];}
 	else                          { fcse = f[j       -nx+1];}
-	
+	//----- ----- ----- ----- ----- ----- ----- ----- ----- ----- #10
 	if(jx == 0) {fcww = f[j+nx-2];}
 	else if(jx == 1){ fcww = f[j+nx-2];}
 	else            { fcww = f[j   -2];}
-	
+	//----- ----- ----- ----- ----- ----- ----- ----- ----- ----- #11
 	if(jx == nx - 2)     { fcee = f[j-nx+2];}
 	else if(jx == nx - 1){ fcee = f[j-nx+2];}
 	else                 { fcee = f[j   +2];}
-	
+	//----- ----- ----- ----- ----- ----- ----- ----- ----- ----- #12
 	if(jy == ny - 2)      { fcnn = f[j-nx*ny+nx+nx];}
 	else if(jy == ny - 1) { fcnn = f[j-nx*ny+nx+nx];}
 	else                  { fcnn = f[j      +nx+nx];}
-	
+	//----- ----- ----- ----- ----- ----- ----- ----- ----- ----- #13
 	if(jy == 0)      { fcss = f[j+nx*ny-nx-nx];}
 	else if(jy == 1) { fcss = f[j+nx*ny-nx-nx];}
 	else             { fcss = f[j      -nx-nx];}
+	//----- ----- ----- ----- ----- ----- ----- ----- ----- ----- 
 	
+	//----- ----- ----- ----- ----- ----- ----- ----- ----- ----- 
+	// term1 = Atomic_interaction*(1-2*f) + RT*{log(f) - log(1-f)}
 	mu_chc = L0*(1.0-2.0*fcc)+rr*temp*(log(fcc)-log(1.0-fcc));
 	mu_chw = L0*(1.0-2.0*fcw)+rr*temp*(log(fcw)-log(1.0-fcw));
 	mu_che = L0*(1.0-2.0*fce)+rr*temp*(log(fce)-log(1.0-fce));
 	mu_chn = L0*(1.0-2.0*fcn)+rr*temp*(log(fcn)-log(1.0-fcn));
 	mu_chs = L0*(1.0-2.0*fcs)+rr*temp*(log(fcs)-log(1.0-fcs));
+	//----- ----- ----- ----- ----- ----- ----- ----- ----- ----- 
 	
+	//----- ----- ----- ----- ----- ----- ----- ----- ----- ----- 
+	// term2 = -gradient_energy_coefficient * Laplacian(f)
 	mu_suc = -kapa_c*(fce +fcw +fcn +fcs -4.0*fcc)/dx/dx;
 	mu_suw = -kapa_c*(fcc +fcww+fcnw+fcsw-4.0*fcw)/dx/dx;
 	mu_sue = -kapa_c*(fcee+fcc +fcne+fcse-4.0*fce)/dx/dx;
 	mu_sun = -kapa_c*(fcne+fcnw+fcnn+fcc -4.0*fcn)/dx/dx;
 	mu_sus = -kapa_c*(fcse+fcsw+fcc +fcss-4.0*fcs)/dx/dx;
+	//----- ----- ----- ----- ----- ----- ----- ----- ----- ----- 
 	
-	mu_c = mu_chc + mu_suc;
-	mu_w = mu_chw + mu_suw;
-	mu_e = mu_che + mu_sue;
-	mu_n = mu_chn + mu_sun;
-	mu_s = mu_chs + mu_sus;
+	//----- ----- ----- ----- ----- ----- ----- ----- ----- ----- 
+	// mu = dG/df = term1 + term2
+	mu_c = mu_chc + mu_suc; // at current (jx,jy) grid point
+	mu_w = mu_chw + mu_suw; // at (jx-1,jy) grid point
+	mu_e = mu_che + mu_sue; // at (jx+1,jy) grid point
+	mu_n = mu_chn + mu_sun; // at (jx,jy+1) grid point
+	mu_s = mu_chs + mu_sus; // at (jx,jy-1) grid point
+	//----- ----- ----- ----- ----- ----- ----- ----- ----- ----- 
 	
+	//----- ----- ----- ----- ----- ----- ----- ----- ----- ----- 
+	// Laplacian(mu) = d^2(mu)/dx^2 + d^2(mu)/dy^2
 	nab_mu = (mu_w + mu_e + mu_n + mu_s -4.0*mu_c)/dx/dx;
-	dfmdx = ((mu_w-mu_e)*(fcw-fce))/(4.0*dx*dx); 
-	dfmdy = ((mu_n-mu_s)*(fcn-fcs))/(4.0*dy*dy); 
+	//----- ----- ----- ----- ----- ----- ----- ----- ----- ----- 
 	
+	//----- ----- ----- ----- ----- ----- ----- ----- ----- ----- 
+	// (df/dx) * d(mu)/dx
+	dfmdx = ((mu_w-mu_e)*(fcw-fce))/(4.0*dx*dx);
+	//----- ----- ----- ----- ----- ----- ----- ----- ----- ----- 
+	// (df/dy) * d(mu)/dy
+	dfmdy = ((mu_n-mu_s)*(fcn-fcs))/(4.0*dy*dy);
+	//----- ----- ----- ----- ----- ----- ----- ----- ----- ----- 
+	
+	//----- ----- ----- ----- ----- ----- ----- ----- ----- ----- 
+	// Mobility, M = { (D_A/RT)*c + (D_B/RT)*(1-c) }*c*(1-c)
+	//             = (D_a/RT)*{f + (D_B/D_A)*(1-f)}*f*(1-f)
 	mcc = (da/rr/temp)*(fcc+dab*(1.0-fcc))*fcc*(1.0-fcc);
+	//----- ----- ----- ----- ----- ----- ----- ----- ----- ----- 
+	
+	//----- ----- ----- ----- ----- ----- ----- ----- ----- ----- 
+	// dM/df
 	dmc = (da/rr/temp)*((1.0-dab)*fcc*(1.0-fcc)
 		+(fcc+dab*(1.0-fcc))*(1.0-2.0*fcc));
+	//----- ----- ----- ----- ----- ----- ----- ----- ----- ----- 
 	
+	//----- ----- ----- ----- ----- ----- ----- ----- ----- ----- 
+	// df/dt = M*Laplacian(f) + (dM/df)*( (df/dx) * d(mu)/dx + (df/dy) * d(mu)/dy )
 	dfdt = mcc*nab_mu + dmc*(dfmdx+dfmdy);
 	fn[j] = f[j]+dfdt*dt;
-	
+	//----- ----- ----- ----- ----- ----- ----- ----- ----- ----- 
 }
 
 void update(float **f, float **fn)
@@ -155,7 +193,7 @@ int main(int argc, char** argv)
 	//copy F_h(cpu,host) to f_d(cuda,device)
 	cudaMemcpy(f_d,F_h,nx*ny*sizeof(float),cudaMemcpyHostToDevice);
 	
-	int BS=16; // Number of threads
+	int BS=32; // Number of threads
 	dim3 blocks(nx/BS,ny/BS,1); //nx*ny = blocks * threads
 	dim3 threads(BS,BS,1);      //BS*BS*1 <= 1024
 	
