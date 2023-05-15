2c84528f74f15204826e04f34f6e7bf7ffd2c176
by-student-2017 <studentsctest@gmail.com>
2023-05-15 09:09:30 +0900

    ubuntu 22.04 lts

--- Takaki_lab_kit/ubuntu_version/Accelerate_phase-field_simulation_with_GPU/main-gpu.cu
+++ Takaki_lab_kit/ubuntu_version/Accelerate_phase-field_simulation_with_GPU/main-gpu.cu
@@ -293,7 +293,7 @@ int main(int argc, char** argv)
 	for(int istep=0; istep<=nstep ; istep++){
 		//calculate subroutine "Kernel" on GPU
 		Kernel<<<blocks, threads>>>(f_d,fn_d,nx,ny,rr,temp,L0,kapa_c,da,db,dt,dx,dy);
-		//cudaThreadSynchronize();
+		cudaDeviceSynchronize(); //<-new version|old version -> cudaThreadSynchronize();
 		
 		// replace f_d with new f_d (=fn_d)
 		update(&f_d,&fn_d);
