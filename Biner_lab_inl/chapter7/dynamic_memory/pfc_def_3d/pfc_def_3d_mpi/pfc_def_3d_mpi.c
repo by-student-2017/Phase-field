7c46f8b795125c6032e771085e4f9100084629c4
by-student-2017 <33742249+by-student-2017@users.noreply.github.com>
2024-06-12 20:40:18 +0900

    Update pfc_def_3d_mpi.c

--- Biner_lab_inl/chapter7/dynamic_memory/pfc_def_3d/pfc_def_3d_mpi/pfc_def_3d_mpi.c
+++ Biner_lab_inl/chapter7/dynamic_memory/pfc_def_3d/pfc_def_3d_mpi/pfc_def_3d_mpi.c
@@ -323,7 +323,7 @@ int main(int argc, char **argv){
 				//output the results in vtk file format for contour plots to be viewed by using paraview
 				write_vtk_grid_values_3D(Nx,Ny,Nz,dx,dy,dz,dx0,istep,den_out,ff_out);
 				
-				printf("done step: %5d \n", istep);
+				printf("done step: %8d \n", istep);
 			}
 		}
 		
